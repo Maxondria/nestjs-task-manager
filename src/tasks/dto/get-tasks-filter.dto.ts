54a0f8254ad4cc88de5088f7e360094e7c883213
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-30 15:31:10 +0300

    validate  query params DTO (filtered DTO)

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
 import { TaskStatus } from '../tasks.model';
+import { IsOptional, IsIn } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDTO {
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
