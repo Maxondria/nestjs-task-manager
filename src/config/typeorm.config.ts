d97635574e748a9cca63364fd9cea2efd1e2362c
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-12-01 23:14:47 +0300

    add app wide config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'tasks_management',
+  type: dbConfig.type,
+  host: process.env.HOST || dbConfig.host,
+  port: process.env.PORT || dbConfig.port,
+  username: process.env.USER || dbConfig.username,
+  password: process.env.PASSWORD || dbConfig.password,
+  database: process.env.DATABASE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
